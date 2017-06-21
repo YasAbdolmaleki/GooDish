9d6e0b6124bd00a5cf7d8255ca35b1e21e50f460
Yas Abdolmaleki <yasabdolmaleki@gmail.com>
2017-06-20 21:01:49 -0400

    created a new project

---
+++ GooDish/GooDish/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GooDish
+//
+//  Created by Yas Abdolmaleki on 2017-06-20.
+//  Copyright © 2017 Yas Abdolmaleki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GooDish/GooDish/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GooDish
+//
+//  Created by Yas Abdolmaleki on 2017-06-20.
+//  Copyright © 2017 Yas Abdolmaleki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
