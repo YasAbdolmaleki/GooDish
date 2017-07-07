e1271d86fe2eda1c68d19d1a7a4c25491a441e5e
Yas Abdolmaleki <yasabdolmaleki@gmail.com>
2017-07-07 00:32:11 -0400

    New cell for the home page

---
+++ GooDish/GooDish/DishHeaderCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DishHeaderCollectionViewCell.h
+//  GooDish
+//
+//  Created by Yas Abdolmaleki on 2017-07-07.
+//  Copyright © 2017 Yas Abdolmaleki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DishHeaderCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ GooDish/GooDish/DishHeaderCollectionViewCell.m
@@ -0,0 +1,18 @@
+//
+//  DishHeaderCollectionViewCell.m
+//  GooDish
+//
+//  Created by Yas Abdolmaleki on 2017-07-07.
+//  Copyright © 2017 Yas Abdolmaleki. All rights reserved.
+//
+
+#import "DishHeaderCollectionViewCell.h"
+
+@implementation DishHeaderCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
