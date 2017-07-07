690a2b71e8f1a7bf0968bb5c7399519ffc3d5a0d
Yas Abdolmaleki <yasabdolmaleki@gmail.com>
2017-07-06 21:36:47 -0400

    Profile view controller is created (skeleton)

---
+++ GooDish/GooDish/ProfileCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileCollectionViewController.h
+//  GooDish
+//
+//  Created by Yas Abdolmaleki on 2017-07-06.
+//  Copyright © 2017 Yas Abdolmaleki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileCollectionViewController : UICollectionViewController
+
+@end
